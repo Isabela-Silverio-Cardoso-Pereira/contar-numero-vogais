e8ae8feab2a39bd22898165a2e7b4d5c32e09764
Isabela-Silverio-Cardoso-Pereira <isabelasilverio.c@gmail.com>
2023-11-05 14:02:40 -0300

    primeiro commit

---
+++ main.cpp
@@ -0,0 +1,58 @@
+
+//4. Imprimir os números ímpares inteiros de X a Y.
+
+
+
+
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+   char letra;
+   int conta_vogal;
+   conta_vogal = 0;
+   for( letra = 'a'; letra <= 'z'; letra++){
+switch(letra){
+
+case 'a':
+case 'e':
+case 'i':
+case 'o':
+case 'u':
+conta_vogal +=letra;
+
+}
+
+
+
+
+
+
+   }
+
+   cout << "o numero de vogais eh " << conta_vogal;
+
+   }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
